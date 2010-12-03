12d634eb41103df24898e2eb27d0c40654ea7ae3
Irwin <coldwater@Merlin.(none)>
2010-12-03 12:15:15 -0800

    I made checkers show a basic window

--- checkers.py
+++ checkers.py
@@ -1,1 +1,47 @@
-print('hi')
+#! /usr/bin/env python
+import sys
+import os
+import pygtk
+import gtk
+
+class Checkers():
+
+    def keypress_cb(self, widget, event):
+        "Respond when the user presses one of the arrow keys"
+        keyname = gtk.gdk.keyval_name(event.keyval)
+        if keyname == 'plus':
+            self.font_increase()
+            return True
+        if keyname == 'minus':
+            self.font_decrease()
+            return True
+        if keyname == 'Page_Up' :
+            self.page_previous()
+            return True
+        if keyname == 'Page_Down':
+            self.page_next()
+            return True
+        if keyname == 'Up' or keyname == 'KP_Up' \
+                or keyname == 'KP_Left':
+            self.scroll_up()
+            return True
+        if keyname == 'Down' or keyname == 'KP_Down' \
+                or keyname == 'KP_Right':
+            self.scroll_down()
+            return True
+        return False
+
+    def destroy_cb(self, widget, data=None):
+        gtk.main_quit()
+
+    def main(self):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.connect("destroy", self.destroy_cb)
+        self.window.set_title("checkers")
+        self.window.set_size_request(640, 480)
+        self.window.set_border_width(0)
+        self.window.show()
+        gtk.main()
+
+if __name__ == "__main__":
+    Checkers().main()
